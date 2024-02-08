91dfa9fa7b6b3e0ab242e3532b0473c0228afdf7
salmanpolito <melleniumster@gmail.com>
2024-02-08 15:34:23 +0900

    Online Courses updated

--- lec_online.js
+++ lec_online.js
@@ -8,3 +8,26 @@ $(document).ready(function () {
         }
     });
 });
+
+$(document).ready(function () {
+    $('#btn_expand_wn').click(function () {
+        const div = $('#wn_div');
+        if (div.hasClass('hidden')) {
+            div.removeClass('hidden');
+        } else {
+            div.addClass('hidden');
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $('#btn_expand_cn').click(function () {
+        const div = $('#cn_div');
+        if (div.hasClass('hidden')) {
+            div.removeClass('hidden');
+        } else {
+            div.addClass('hidden');
+        }
+    });
+});
